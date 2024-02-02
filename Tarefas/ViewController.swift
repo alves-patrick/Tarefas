ee22d7f308baa279a6a254d0f7f00b1f2a52a281
Patrick Alves <patrickalvesarts@gmail.com>
2024-02-02 15:47:14 -0300

    Initial Commit

---
+++ Tarefas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tarefas
+//
+//  Created by Patrick Alves on 2/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
